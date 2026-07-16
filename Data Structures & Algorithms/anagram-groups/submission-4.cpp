1fd601e2ec5bd6018348eedc2c5aee4e87bde562
Taj Macé <108613034+tajMace@users.noreply.github.com>
2026-07-17 00:06:26 +1000

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,45 @@
+string getLetterFrequencyArray(const string& str);
+vector<vector<string>> indexToArray(const vector<string>& strs, const unordered_map<string,vector<int>>& seen);
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<int>> seen;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string freq = getLetterFrequencyArray(strs[i]);
+            vector<int>& v = seen[freq];
+            v.push_back(i);
+        }
+
+        return indexToArray(strs, seen);
+    }
+};
+
+string getLetterFrequencyArray(const string& str) {
+    vector<int> v(26);
+    for (auto &letter : str) {
+        v[letter - 'a']++;
+    }
+
+    string key;
+    for (int count : v) {
+        key += to_string(count) + "#";
+    }
+
+    return key;
+}
+
+vector<vector<string>> indexToArray(const vector<string>& strs, const unordered_map<string,vector<int>>& seen) {
+    vector<vector<string>> result;
+
+    for (auto &[vec, idxs] : seen) {
+        vector<string> same;
+        for (auto &strIdx : idxs) {
+            same.push_back(strs[strIdx]);
+        }
+        result.push_back(same);
+    }
+
+    return result;
+}
