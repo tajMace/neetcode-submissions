bdf2a5aed35873b638abf1de806f7de96245b7e0
Taj Macé <108613034+tajMace@users.noreply.github.com>
2026-07-16 23:29:09 +1000

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> seen;         // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int compliment = target - nums[i];
+
+            if (seen.count(compliment)) {
+                return {seen[compliment], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+    }
+};
