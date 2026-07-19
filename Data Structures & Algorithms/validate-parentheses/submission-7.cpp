1eccf213851eaf68ac5eae1c4b5db9459a35ed65
Taj Macé <108613034+tajMace@users.noreply.github.com>
2026-07-19 11:36:05 +1000

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> open;
+        for (auto &c : s) {
+            if (isOpeningBracket(c)) {
+                open.push(c);
+            } else if (isValidClosing(c, open)) {
+                open.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return open.empty() ? true : false;
+    }
+
+private:
+    bool isValidClosing(const char c, stack<char>& open) const {
+        return (
+            isClosingBracket(c) && 
+            !open.empty() && 
+            open.top() == getMatch(c)
+        );
+    }
+
+    bool isOpeningBracket(const char c) const {
+        return (
+            c == '(' ||
+            c == '[' ||
+            c == '{'
+        );
+    }
+
+    bool isClosingBracket(const char c) const {
+        return (
+            c == ')' ||
+            c == ']' ||
+            c == '}'
+        );
+    }
+
+    char getMatch(const char c) const {
+        if (c == ')') return '(';
+        if (c == ']') return '[';
+        if (c == '}') return '{';
+        return 0;
+    }
+};
+
+
+
