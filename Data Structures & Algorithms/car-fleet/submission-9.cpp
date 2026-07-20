589664f16ead119e6dcd33d2d1e46d397096f9d7
Taj Macé <108613034+tajMace@users.noreply.github.com>
2026-07-20 14:09:33 +1000

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int size = position.size();
+        if (size == 1) {
+            return 1;
+        }
+
+        // translate to pairs
+        vector<pair<int, int>> cars(size);
+        for (int i = 0; i < size; i++) {
+            cars[i] = { position[i], speed[i] };
+        }
+
+        // sort pairs based on starting position -> end to front
+        sort(cars.begin(), cars.end(), [](const auto &a, const auto &b) {
+            return a.first > b.first;
+        });
+
+        // iterate forwards, counting 'groups'
+        int groups = 0;
+        double fleetTime = 0;
+        for (int i = 0; i < size; i++) {
+            double endTime = (double)(target - cars[i].first) / cars[i].second;
+
+            if (endTime <= fleetTime) {
+                continue;
+            }
+
+            groups++;
+            fleetTime = endTime;
+        }
+
+        return groups;
+
+    }
+};
